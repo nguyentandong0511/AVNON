9f49f88c5becb6a020a13be34f8d9d4a5c086ad5
dong <nguyentandong0511@gmail.com>
2023-09-27 22:10:31 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <h1>hello</h1>
+    <router-outlet></router-outlet>
+  `,
   standalone: true,
   imports: [RouterOutlet]
 })
